fde23c483af6ed23a9dc7fc0ad47cb2d02c38056
varsanydavid20 <varsanyid.24d@acsjszki.hu>
2025-04-03 08:39:49 +0200

    Update nyomtatas.js

--- nyomtatas.js
+++ nyomtatas.js
@@ -1,15 +1,17 @@
 function kalkulal(){
-      const szelesseg=297;
-    const magassag=420;
-
-    //Űrlapadatok
+   //Űrlapalapok
+    const szelesseg=document.getElementById('szelesseg').valua;
+    const magassag=document.getElementById('magassag').value;
     const papir=document.getElementById('papirtipus').value;
 
     //Számítások
     let terulet=Math.round((szelesseg*magassag)/10000);    
-    let koltseg=terulet*papir;
+    let koltseg=terulet * papir;
     
     //Megjelenítés
+    document.getElementById('terulet').innerHTML=terulet;
+    document.getElementById('koltseg').innerHTML=koltseg
+
     document.getElementById('valasz').style.visibility = "visible";
 }
 
